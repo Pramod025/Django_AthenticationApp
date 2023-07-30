5be13a9dd864ab63dc90bfef2e66521b9ff21278
Pramod <57028365+Pramod025@users.noreply.github.com>
2023-07-30 19:38:51 +0530

    Update info.py
    Change of email and password

--- project2/info.py
+++ project2/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'djnago.test.email@gmail.com'
-EMAIL_HOST_PASSWORD = 'xwfozgfegvjhmerk'
+EMAIL_HOST_USER = '*******************'
+EMAIL_HOST_PASSWORD = '************'
 EMAIL_PORT = 587
